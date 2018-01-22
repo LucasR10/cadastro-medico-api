1ca7d7e0ee6886478709ef87f9f6de5b410ba84d
Lucas <lucas.carvalho@basis.com.br>
2018-01-22 18:26:43 -0200

    ......

--- src/main/java/br/com/fullstack/repository/MedicoRepository.java
+++ src/main/java/br/com/fullstack/repository/MedicoRepository.java
@@ -1,9 +1,10 @@
 package br.com.fullstack.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.fullstack.entities.Medico;
 
-public interface MedicoRepository extends JpaRepository<Medico, Long> {
+public interface MedicoRepository 	extends JpaRepository<Medico, Long> {
 
 }
